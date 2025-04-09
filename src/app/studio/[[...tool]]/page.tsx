4b699561afc6297ffb9aad3ec9dd7ccc41827071
johnwaterstag <brownjamie599@gmail.com>
2025-04-09 18:25:50 +0100

    fix: update Sanity Studio page configuration to properly handle context

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -9,11 +9,16 @@
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
+import { StudioProvider } from 'sanity'
 
 export const dynamic = 'force-static'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return (
+    <StudioProvider config={config}>
+      <NextStudio config={config} />
+    </StudioProvider>
+  )
 }
